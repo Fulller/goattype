7dade170ed605d69bbf274e2b597f15be47c7961
CopperFull <nguyenday3332@gmail.com>
2023-05-04 10:46:06 +0700

    f1

--- js/tools.js
+++ js/tools.js
@@ -9,6 +9,7 @@ export function checkLogin() {
   }
   let isLogin = getIsLogin("isLogin", false);
   const pathname = window.location.pathname;
+  console.log(window.location);
   if (pathname.includes("/login.html")) {
     if (isLogin) {
       window.location.replace("index.html");
