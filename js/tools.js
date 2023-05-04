6e24b5e94c722cf2eae573ee146348a05e0b99b4
CopperFull <nguyenday3332@gmail.com>
2023-05-04 10:41:46 +0700

    fix checkLogin

--- js/tools.js
+++ js/tools.js
@@ -9,7 +9,7 @@ export function checkLogin() {
   }
   let isLogin = getIsLogin("isLogin", false);
   const pathname = window.location.pathname;
-  if (["/login.html"].includes(pathname)) {
+  if (pathname.includes("/login.html")) {
     if (isLogin) {
       window.location.replace("index.html");
     }
